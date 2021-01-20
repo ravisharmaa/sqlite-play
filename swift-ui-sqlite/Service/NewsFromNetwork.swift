6d080b00dc038e022717fc2be50e0fe5b5f1f754
aloksbd <30898579+aloksbd@users.noreply.github.com>
2021-01-20 17:02:06 +0545

    Create 'NewsFromNetwork' -(Write test first - and create the class)

---
+++ swift-ui-sqlite/Service/NewsFromNetwork.swift
@@ -0,0 +1,12 @@
+//
+//  NewsFromNetwork.swift
+//  swift-ui-sqlite
+//
+//  Created by Alok Subedi on 20/01/2021.
+//
+
+import Foundation
+
+class NewsFromNetwork{
+    
+}
---
+++ swift-ui-sqliteTests/NewsFromNetworkTests.swift
@@ -0,0 +1,16 @@
+//
+//  NewsFromNetworkTests.swift
+//  swift-ui-sqliteTests
+//
+//  Created by Alok Subedi on 20/01/2021.
+//
+
+import XCTest
+@testable import swift_ui_sqlite
+
+class NewsFromNetworkTests: XCTestCase {
+    func test_init_doesNotRequestDataFromUrl(){
+        let sut = NewsFromNetwork()
+    }
+}
+
