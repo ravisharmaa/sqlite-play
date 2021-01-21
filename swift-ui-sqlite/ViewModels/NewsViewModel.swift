f2d52054a507da05f27a98603c3d7e2268305586
aloksbd <30898579+aloksbd@users.noreply.github.com>
2021-01-21 17:25:53 +0545

    Move urlComponents to NewsFromNetwork

--- swift-ui-sqlite/ViewModels/NewsViewModel.swift
+++ swift-ui-sqlite/ViewModels/NewsViewModel.swift
@@ -11,13 +11,6 @@ import Combine
 final class NewsViewModel {
     var subscription: Set<AnyCancellable> = []
     
-    var urlComponents: URLComponents  {
-        var component = URLComponents()
-        component.scheme = "https"
-        component.host = "newsapi.org"
-        return component
-    }
-    
     @Published private (set) var articles: [Article] = []
     
     func fetch() {
@@ -25,27 +18,8 @@ final class NewsViewModel {
     }
     
     fileprivate func fromNetwork() {
-        
-        var innerUrl = urlComponents
-        
-        innerUrl.path = "/v2/top-headlines"
-        
-        var urlQueryItem: [URLQueryItem] = []
-        
-        let queryItems: [String: String]  = [
-            "country": "in",
-            "category": "business",
-            "apiKey": "721e810f5e984c8b8529b6816ad464b4"
-        ]
-        
-        for (key, data) in queryItems  {
-            urlQueryItem.append(.init(name:key, value: data))
-        }
-        
-        innerUrl.queryItems = urlQueryItem
-        
         let newsFromNetwork = NewsFromNetwork(subscription: subscription)
-        newsFromNetwork.fetch(request: URLRequest(url: innerUrl.url!)){ result in
+        newsFromNetwork.fetch(request: nil){ result in
             switch result{
             case let .success(news):
                 self.articles = news.articles
